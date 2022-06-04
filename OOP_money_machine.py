c2033134de01682cbd48e7530b7be9837179f26b
Brendan Larson <67674009+brendanlarson98@users.noreply.github.com>
2022-06-04 14:29:42 -0500

    Create OOP_money_machine.py

---
+++ OOP_money_machine.py
@@ -0,0 +1,44 @@
+class MoneyMachine:
+
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money:", '${:,.2f}'.format(self.profit))
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            not_valid = True
+            while not_valid:
+                try:
+                    self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+                    not_valid = False
+                except ValueError:
+                    print("That wasn't a valid input. Please enter an integer number")
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        print(f"Cost:", '${:,.2f}'.format(cost))
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is", '${:,.2f}'.format(change), "in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
